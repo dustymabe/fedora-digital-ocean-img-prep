dbf689f859203125dbd7de62bfc25b764ec26174
Dusty Mabe <dusty@dustymabe.com>
2017-06-05 08:19:46 -0400

    DO-F25a: Update to candidate 20170605

--- DigitalOcean/Fedora/Fedora25-Atomic-Host/processimg.sh
+++ DigitalOcean/Fedora/Fedora25-Atomic-Host/processimg.sh
@@ -40,9 +40,9 @@ WORKDIR=/workdir
 TMPMNT=/workdir/tmp/mnt
 
 # Vars for the image
-XZIMGURL='https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-Atomic-25-20170522.0/CloudImages/x86_64/images/Fedora-Atomic-25-20170522.0.x86_64.raw.xz'
+XZIMGURL='https://kojipkgs.fedoraproject.org/compose/twoweek/Fedora-Atomic-25-20170605.0/compose/CloudImages/x86_64/images/Fedora-Atomic-25-20170605.0.x86_64.raw.xz'
 XZIMG=$(basename $XZIMGURL) # Just the file name
-XZIMGSUM='2508e72967aebe46650f405746aba7f042173172d6d10c68826305a1677e4c6b'
+XZIMGSUM='eea6bd1bcdba75d954f9d794de9915b1bce9190b9b08c7616372c410d39e0016'
 IMG=${XZIMG:0:-3}           # Pull .xz off of the end
 
 # Create workdir and cd to it
