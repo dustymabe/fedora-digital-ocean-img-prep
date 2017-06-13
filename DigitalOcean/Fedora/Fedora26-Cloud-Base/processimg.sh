56d0b5c545de8f9e03c297ed57e21cf9834e7632
Dusty Mabe <dusty@dustymabe.com>
2017-06-13 09:33:37 -0400

    DO-F26: First stab at f26 beta cloud base

---
+++ DigitalOcean/Fedora/Fedora26-Cloud-Base/processimg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free  Software Foundation; either version 2 of the License, or
+# (at your option)  any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
+#
+#
+# purpose: This script will download a fedora image and then modify it 
+#          to prepare it for Digital Ocean's infrastructure. It uses 
+#          Docker to hopefully guarantee the behavior is consistent across 
+#          different machines.
+#  author: Dusty Mabe (dusty@dustymabe.com)
+
+set -eux 
+mkdir -p /tmp/doimg/
+cd /tmp/doimg
+
+IMGURL='https://download.fedoraproject.org/pub/fedora/linux/releases/test/26_Beta/CloudImages/x86_64/images/Fedora-Cloud-Base-26_Beta-1.4.x86_64.qcow2'
+IMGSUM='dd8f0599ac6243ba6f267759985aba64aa51ea4e26a61e297f8aae6bb6a019b2'
+
+# Get the xz image, verify, and decompress the contents
+curl -O $IMGURL
+imgsum=$(sha256sum $IMG | cut -d " " -f 1)
+if [ "$imgsum" != "$IMGSUM" ]; then
+    echo "Checksum doesn't match: $imgsum"
+    exit 1
+fi
