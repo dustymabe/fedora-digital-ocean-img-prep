f5509fff7be0a8e6e5c7f2d19372ae1db06a2ffa
Dusty Mabe <dusty@dustymabe.com>
2017-07-07 17:43:08 -0400

    DO-F26a: release candidate for F26AH

--- DigitalOcean/Fedora/Fedora26-Atomic-Host/processimg.sh
+++ DigitalOcean/Fedora/Fedora26-Atomic-Host/processimg.sh
@@ -22,90 +22,17 @@
 #          different machines.
 #  author: Dusty Mabe (dusty@dustymabe.com)
 
-set -eu 
+set -eux 
 mkdir -p /tmp/doimg/
 cd /tmp/doimg
 
-# need wget/sha256sum/unxz for this script
-#   for exe in wget sha256sum unxz; do
-#       if ! which $exe >/dev/null; then
-#           echo "You need $exe to run this program"
-#           exit 1
-#       fi
-#   done
-
-docker run -i --rm --privileged -v /tmp/doimg:/tmp/doimg fedora:25 bash << 'EOF'
-set -eux
-WORKDIR=/workdir
-TMPMNT=/workdir/tmp/mnt
-
-# Vars for the image
-XZIMGURL='https://s3.amazonaws.com/fedora-atomic-s3/Fedora-26-20170611.n.1/Fedora-Atomic-26-20170611.n.1.x86_64.raw.xz'
-XZIMG=$(basename $XZIMGURL) # Just the file name
-XZIMGSUM='3c327f52386afb3684140a52522384eded239f0cde9e2dba9419f03fbbe35d70'
-IMG=${XZIMG:0:-3}           # Pull .xz off of the end
-
-# Create workdir and cd to it
-mkdir -p $TMPMNT && cd $WORKDIR
-
-# Get any additional rpms that we need
-dnf install -y wget xz lvm2
+IMGURL='https://kojipkgs.fedoraproject.org/compose/twoweek/Fedora-Atomic-26-20170707.1/compose/CloudImages/x86_64/images/Fedora-Atomic-26-20170707.1.x86_64.qcow2'
+IMGSUM='d342c0923b57090c60f2df26bb7a0531924ee00b66f2d05ddf32e0741ad032bb'
 
 # Get the xz image, verify, and decompress the contents
-wget $XZIMGURL
-imgsum=$(sha256sum $XZIMG | cut -d " " -f 1)
-if [ "$imgsum" != "$XZIMGSUM" ]; then
+curl -O $IMGURL
+imgsum=$(sha256sum $IMG | cut -d " " -f 1)
+if [ "$imgsum" != "$IMGSUM" ]; then
     echo "Checksum doesn't match: $imgsum"
     exit 1
 fi
-unxz $XZIMG
-
-
-# Discover the next available loopback device
-LOOPDEV=$(losetup -f)
-LOMAJOR=''
-
-# Make the loopback device if it doesn't exist already
-if [ ! -e $LOOPDEV ]; then
-    LOMAJOR=${LOOPDEV#/dev/loop} # Get just the number
-    mknod -m660 $LOOPDEV b 7 $LOMAJOR
-fi
-
-# Find the starting byte and the total bytes in the 2nd partition
-# We only care about the 2nd partition - the first partition is /boot
-PAIRS=$(partx --pairs $IMG)
-eval `echo "$PAIRS" | tail -n 1 | sed 's/ /\n/g'`
-STARTBYTES=$((512*START))   # 512 bytes * the number of the start sector
-TOTALBYTES=$((512*SECTORS)) # 512 bytes * the number of sectors in the partition
-
-# Loopmount the 2nd partition of the device
-losetup -v --offset $STARTBYTES --sizelimit $TOTALBYTES $LOOPDEV $IMG
-
-# Tell lvm to not depend on udev and create device nodes itself.
-#sed -i 's/udev_sync = 1/udev_sync = 0/' /etc/lvm/lvm.conf
-#sed -i 's/udev_rules = 1/udev_rules = 0/' /etc/lvm/lvm.conf
-
-# Enable volume group
-pvscan
-vgchange -a y atomicos
-vgscan --mknodes
-
-# Mount it on $TMPMNT
-mount /dev/mapper/atomicos-root $TMPMNT
-
-# Disable NM and enable network
-chroot ${TMPMNT}/ostree/deploy/fedora-atomic/deploy/*.0/ <<IEOF
-echo -e '[main]\ndns=none' > /etc/NetworkManager/conf.d/dont-touch-resolvconf.conf
-IEOF
-
-
-# umount and tear down loop device
-umount $TMPMNT
-vgchange -a n atomicos
-losetup -d $LOOPDEV
-[ ! -z $LOMAJOR ] && rm -f $LOOPDEV #Only remove if we created it
-
-# finally, cp $IMG into /tmp/doimg/ on the host
-cp -a $IMG /tmp/doimg/ 
-
-EOF
